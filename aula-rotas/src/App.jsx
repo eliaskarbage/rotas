9d0eb17f9294fa1894d54204353568743783b8a2
Seu nome aqui <seuemail@email.com>
2025-05-13 20:04:42 -0300

    criando a estrutura de layout vite

---
+++ aula-rotas/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <p>Aula: Rotas</p>
+        <button>Home</button>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
